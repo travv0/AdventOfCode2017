2c2ea0f530f604720f06ec07e7aea1546f820180
Travis <tssund93@gmail.com>
2022-01-14 01:01:32 -0600

    day 12

--- day12/day12.hs
+++ day12/day12.hs
@@ -1,3 +1,9 @@
+#!/usr/bin/env -S cabal run -v0
+{- cabal:
+build-depends: base, split, containers
+ghc-options: -Wall
+-}
+
 import qualified Data.List.Split               as List
 import           Data.Map                       ( Map )
 import qualified Data.Map                      as Map
@@ -5,36 +11,45 @@ import           Data.Set                       ( Set )
 import qualified Data.Set                      as Set
 import           GHC.Exts                       ( fromList )
 
-newtype Program = Program Int deriving (Show, Eq, Ord)
-newtype Pipes = Pipes (Map Program (Set Program)) deriving Show
+type Program = Int
+type ProgramGroup = Set Program
+type Pipes = Map Program ProgramGroup
 
 parse :: String -> Pipes
 parse =
-    Pipes
-        . fromList
+    fromList
         . map
               (\line ->
                   let [program, connections] = List.splitOn " <-> " line
-                  in  ( Program (read program)
-                      , fromList
-                          . map (Program . read)
-                          . List.splitOn ", "
-                          $ connections
+                  in  ( read program
+                      , fromList . map read . List.splitOn ", " $ connections
                       )
               )
         . lines
 
-programsInGroup :: Pipes -> Program -> Set Program
-programsInGroup (Pipes pipes) = go Set.empty
+programsInGroup :: Pipes -> Program -> ProgramGroup
+programsInGroup pipes = go Set.empty
   where
-    go result program = if Set.member program result
-        then result
-        else foldMap (go $ Set.insert program result) $ pipes Map.! program
+    go result program
+        | Set.member program result
+        = result
+        | otherwise
+        = foldMap (go $ Set.insert program result) $ pipes Map.! program
+
+programGroups :: Pipes -> Set ProgramGroup
+programGroups pipes =
+    foldr (Set.insert . programsInGroup pipes) Set.empty $ Map.keys pipes
 
 main :: IO ()
 main = do
     pipes <- parse <$> readFile "input.txt"
+
     putStrLn
         $  "There are "
-        <> show (length $ programsInGroup pipes (Program 0))
+        <> show (length $ programsInGroup pipes 0)
         <> " programs in the group that contains program ID 0"
+
+    putStrLn
+        $  "There are "
+        <> show (length $ programGroups pipes)
+        <> " groups in total"
